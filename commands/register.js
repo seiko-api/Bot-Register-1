4520f3af9ad9be02cf3d49505b7be3606545f055
Ichinose <86679476+seiko-api@users.noreply.github.com>
2022-09-11 00:27:34 +0700

    Update register.js

--- commands/register.js
+++ commands/register.js
@@ -5,17 +5,16 @@ module.exports = {
     description: 'Ini Coding register',
     async execute(message, args, client) {
         if(message.channel.type == "dm") return message.channel.send("Kamu tidak bisa registrasi dari DM.");
-        if (message.channel.id != `${ID_CHANNEL}`) return message.channel.send(`Kamu bisa menggunakan command ini di <#${ID_CHANNEL}>`);
-        if(!args.length) return message.channel.send(`${message.author} **Denied**,***Tolong tulis nama kamu.***`);
+        if (message.channel.id != `${ID_CHANNEL}`) return message.channel.send(`Kamu Menggunakan Command Ini Di Channel Yang Salah, Berikut Channel Yang Benar <#${ID_CHANNEL}>`);
+        if(!args.length) return message.channel.send(`${message.author} **Denied**, __GAPUNYA NAMA APA GIMANA SI ANJING?__`);
 
         const nickname = args.join(" ");
         const private = client.channels.cache.get(`${ID_CHANNEL}`);
 
         try {
             message.member.roles.add(`${ID_ROLE}`);
-            message.member.roles.remove(`${ID_ROLE_REMOVE}`);
             message.member.setNickname(`${TAG} ${nickname}`);
-            private.send(`${message.author} **Accept**, ***Kamu sudah menjadi ${TAG} di ${message.guild.name}.***`);
+            private.send(`${message.author} **Accepted**, __Jangan Lupa Order Product Legends Store Ya!!!__`);
         } catch(error) {
             console.error(error); 
         }
