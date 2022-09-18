f90e238464762a0e6c443d747c67e1b645749a86
Ichinose <86679476+seiko-api@users.noreply.github.com>
2022-09-19 02:49:14 +0700

    Update register.js

--- commands/register.js
+++ commands/register.js
@@ -4,17 +4,16 @@ module.exports = {
     name: 'register',
     description: 'Ini Coding register',
     async execute(message, args, client) {
-        if(message.channel.type == "dm") return message.channel.send("Kamu tidak bisa registrasi dari DM.");
-        if (message.channel.id != `${ID_CHANNEL}`) return message.channel.send(`Kamu Menggunakan Command Ini Di Channel Yang Salah, Berikut Channel Yang Benar <#${ID_CHANNEL}>`);
-        if(!args.length) return message.channel.send(`${message.author} **Denied**, __GAPUNYA NAMA APA GIMANA SI ANJING?__`);
+        if(message.channel.type == "dm") return
+        if (message.channel.id != `${ID_CHANNEL}`) return
+        if(!args.length) return
 
         const nickname = args.join(" ");
         const private = client.channels.cache.get(`${ID_CHANNEL}`);
 
         try {
             message.member.roles.add(`${ID_ROLE}`);
-            message.member.setNickname(`${TAG} ${nickname}`);
-            private.send(`${message.author} **Accepted**`);
+            message.member.setNickname(`${nickname}`);
         } catch(error) {
             console.error(error); 
         }
